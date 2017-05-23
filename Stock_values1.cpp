115b3e56308f018a49a556e4874eee8385172f11
amitjjaiswal <amitjaiswal.ajj.96@gmail.com>
2017-05-23 18:43:11 +0530

    Add files via upload

---
+++ Stock_values1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+long int totalcost;
+
+long int fun(int pc,int j)
+{
+    long int cost1,cost2;
+    if(i>n)
+        return(0);
+    if(i>1 && i<n)
+    {
+        if(pc%2==1 or p[i+1]%2==1)
+        {
+
+            cost1=fun(j+1)
+
+int main()
+{
+    int t,n;
+    int p[25];
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&n);
+        i=1;
+        while(i<=n)
+        {
+            scanf("%d",&p[i]);
+            i++;
+        }
